a49f2072a05382d0780fd0c39110d95f65e4a715
GreyWolfWild <I123456789I1I1I@gmail.com>
2016-05-20 19:02:52 +0600

    Добавлено условие для вырожденного случая

--- main.c
+++ main.c
@@ -8,19 +8,34 @@ int main()
 	int a, b, c;
 	scanf("%d %d %d", &a, &b, &c);
 	long double discriminant = b*b - 4*a*c;
-
-	if (discriminant > 0)
+	if (a == 0)
 	{
-		printf("D > 0; 2 корня:\n");
-		printf("x1 = %lf\n", (-b + sqrt(discriminant))/(2*a));
-		printf("x2 = %lf\n", (-b + sqrt(discriminant))/(2*a));
+		if (b != 0)
+		{
+			printf("a = 0;\nУравнение вырождается в линейное:\n");
+			printf("x = %lf\n", -1.*c/b);
+		}
+		else 
+			if (c == 0)
+				printf("Все коэффициенты равны нулю; x - любое число.\n");
+			else
+				printf("Нет решений.\n");
 	}
-	else if (discriminant == 0)
+	else
 	{
-		printf("D = 0; 1 корень:\n");
-		printf("x = %lf", -1.*b/(2*a));
+		if (discriminant > 0)
+		{
+			printf("D > 0; 2 корня:\n");
+			printf("x1 = %lf\n", (-b + sqrt(discriminant))/(2*a));
+			printf("x2 = %lf\n", (-b + sqrt(discriminant))/(2*a));
+		}
+		else if (discriminant == 0)
+		{
+			printf("D = 0; 1 корень:\n");
+			printf("x = %lf", -1.*b/(2*a));
+		}
+		else if (discriminant < 0)
+			printf("D < 0; нет корней.\n");
 	}
-	else if (discriminant < 0)
-		printf("D < 0; нет корней.\n");
-	return 0;
+		return 0;
 }
